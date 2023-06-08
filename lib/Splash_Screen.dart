719d793d467c7a686a196b9b087f50f18c098b60
jeet pavashiya <jeetpasashiya@jeets-Mac-mini.local>
2023-06-08 10:29:10 +0530

    Initial commit - 08-06-2023

---
+++ lib/Splash_Screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:test_git/main.dart';
+
+class Splash_Screen extends StatefulWidget {
+  const Splash_Screen({super.key});
+
+  @override
+  State<Splash_Screen> createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("Splash"),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHomePage(title: ""),
+          ));
+    });
+    super.initState();
+  }
+}
